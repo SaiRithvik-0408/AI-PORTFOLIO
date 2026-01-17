ca80e225e2ea111c2fbac6ff13ff8bcfb10d927c
SaiRithvik-0408 <karnatisairithvik@gmail.com>
2026-01-17 16:49:41 +0530

    feat: add project image exports

---
+++ src/assets/project-images/index.js
@@ -0,0 +1,4 @@
+// Export project images for AI Portfolio
+export { default as aiPortfolioImg1 } from './1.png';
+export { default as aiPortfolioImg2 } from './2.png';
+export { default as aiPortfolioImg3 } from './3.png';
